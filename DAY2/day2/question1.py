d790a3d2dc0176c7c124e2bf0e7eb5c21fb218eb
Sohini Lahiri <sohinilahiri9@gmail.com>
2026-06-10 14:43:08 +0530

    add day2 programs

---
+++ DAY2/day2/question1.py
@@ -0,0 +1,15 @@
+num = int(input("Enter a number: "))
+
+temp = num
+
+sum_digits = 0
+
+while num > 0:
+
+digit = num % 10
+
+sum_digits += digit
+
+num //= 10
+
+print(f"Sum of digits of {temp} = {sum_digits}")
