1d00be83cac8731fb3e3bc0a3655a3cc68856a4b
Sohini Lahiri <sohinilahiri9@gmail.com>
2026-06-30 23:28:51 +0530

    completed

---
+++ 5day/Q4.PY
@@ -0,0 +1,15 @@
+import math
+
+num = int(input("Enter a number: "))
+temp = num
+sum_fact = 0
+
+while temp > 0:
+    digit = temp % 10
+    sum_fact += math.factorial(digit)
+    temp //= 10
+
+if sum_fact == num:
+    print(f"{num} is a strong number")
+else:
+    print(f"{num} is not a strong number")
